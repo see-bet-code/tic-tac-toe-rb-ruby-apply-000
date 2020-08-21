c9c1b30fc0181610b783fe8ca1401d0714de5d88
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-21 01:48:01 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,98 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle row
+  [6,7,8], #Bottom row
+  [0,3,6], #First column
+  [1,4,7], #Second column
+  [2,5,8], #Third column
+  [0,4,8], #Diagonal
+  [2,4,6]  #Diagonal 2
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, token)
+  board[index] = token
+end
+
+def position_taken?(board, index)
+  ["X","x","O","o"].include?(board[index])
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0,8)
+end
+
+def turn_count(board)
+  board.count{|position| !(position == " " || position == "")}
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def won?(board)
+  win = WIN_COMBINATIONS.select{|win|
+    board.values_at(*win).all?("X") || board.values_at(*win).all?("O")}
+  return win.first
+end
+
+def full?(board)
+  !board.include?(" ")
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  index = won?(board)
+  if index == nil
+    nil
+  else
+    board[index.first]
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+
+end
