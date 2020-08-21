af76e22fee6900355c8d9dd2ee910f23b5c99b7c
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-21 01:22:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,3 +44,14 @@ def current_player(board)
     "O"
   end
 end
+
+def turn(board)
+  user_input = get.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
